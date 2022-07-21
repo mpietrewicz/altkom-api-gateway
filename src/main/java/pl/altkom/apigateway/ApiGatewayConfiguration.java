7af1ae4791dc9bcc958eb91298e9463322c6b266
mpietrewicz <mpietrewicz@gmail.com>
2022-07-21 16:01:12 +0200

    Dodanie przekierwoania

---
+++ src/main/java/pl/altkom/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,18 @@
+package pl.altkom.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p.path("/get").uri("http://httpbin.org:80"))
+                .build();
+    }
+}
+
